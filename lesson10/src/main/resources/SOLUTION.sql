63ff6a0f88e32970e0706353b74949bed25b3cdf
Artem Pasko <guramibox777@gmail.com>
2022-11-29 16:42:04 +0300

    add lesson10 solution

---
+++ lesson10/src/main/resources/SOLUTION.sql
@@ -0,0 +1,2 @@
+SELECT s.id, s.description, s.grade FROM subject AS s JOIN mark AS m ON s.id = m.subject_id GROUP BY (s.id) HAVING AVG(m.mark) > (SELECT AVG(mark) FROM mark);
+SELECT s.id, s.name, s.birthday, s.groupnumber FROM student AS s JOIN payment AS p ON s.id = p.student_id GROUP BY (s.id) HAVING AVG(p.amount) < (SELECT AVG(amount) FROM payment);
